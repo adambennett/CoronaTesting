119ab85f1ad51fc67728b89aacc28df2f1fa1574
Adam Bennett <nyoxidetwitter@gmail.com>
2020-12-26 21:41:53 -0500

    Working patient model

---
+++ corona-testing-server/src/entities/patient/patient.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import {Patient} from './patient.entity';
+
+@Injectable()
+export class PatientService {
+    constructor(
+        @InjectRepository(Patient)
+        private patientRepository: Repository<Patient>,
+    ) {}
+
+    findAll(): Promise<Patient[]> {
+        return this.patientRepository.find();
+    }
+
+    findOne(id: string): Promise<Patient> {
+        return this.patientRepository.findOne(id);
+    }
+
+    async remove(id: string): Promise<void> {
+        await this.patientRepository.delete(id);
+    }
+}
