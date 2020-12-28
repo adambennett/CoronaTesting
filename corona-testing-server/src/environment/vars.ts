a2ba3f727a6c3eb95d5800700bbc8aefa82b8d6a
Adam Bennett <nyoxidetwitter@gmail.com>
2020-12-27 20:53:11 -0500

    Set environment variables

--- corona-testing-server/src/environment/vars.ts
+++ corona-testing-server/src/environment/vars.ts
@@ -1,7 +1,7 @@
 export default {
-    DB_PORT: 3306,
-    DB_HOST: '192.168.1.31',
-    DB_USER: 'root',
-    DB_PASS: 'admin',
-    DB_NAME: 'covid_testing'
+    DB_PORT: process.env.DATABASE_PORT,
+    DB_HOST: process.env.DATABASE_HOST_URL,
+    DB_USER: process.env.DATABASE_USERNAME,
+    DB_PASS: process.env.DATABASE_PASSWORD,
+    DB_NAME: process.env.DATABASE_NAME
 }
